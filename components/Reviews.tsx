df65984a1d412341fa025b153ba37b6bd6ee731f
dbackco-design <fdbackco@gmail.com>
2025-08-18 14:21:42 +0900

    rrr

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -18,7 +18,7 @@ const reviews: IReview[] = [
   {
     productName: "HOID 무선청소기",
     modelName: "HO-AC2506",
-    content: "HOID 제품 처음 써보는데 가격대비 너무 좋습니다. 디자인도 깔끔하고 성능은 생각했던 것보다 더 좋네요. A/S센터도 있어서 만약 문제 생겨도 걱정 없더라구요. HOID 다른 제품도 사보려구요...",
+    content: "침대 밑도 FLEX 기능 덕에 쏙쏙 들어가서 너무 좋아요. 육아하느라 허리 아픈데 진짜 신세계... 청소 시간이 확 줄었어요😊",
     image: "/review_4.jpg",
     b_image: "/review_b_3.png",
     href: "https://www.coupang.com/vp/products/8987740925?itemId=26323251903&vendorItemId=93300600462&q=%ED%98%B8%EC%9D%B4%EB%93%9C&searchId=a628d9892153819&sourceType=search&itemsCount=35&searchRank=1&rank=1"
